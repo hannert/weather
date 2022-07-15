e06fcf4206ff3bb960ff478512cf877124c454b2
han.lin <han.lin@mtahq.org>
2022-07-14 20:04:54 -0400

    New Components
    - Button component
    - Display component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
 import potion from './potion.svg'
 import './App.css';
-import Weather from './Weather.tsx'
 import Data from './Data.tsx'
+import Back from './Back.tsx'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Back />
         <img src={potion} className="App-logo" alt="logo" />
+        <div className="App-logo">Wait I love monster hunter</div>
       </header>
 
       <Data />
---
+++ src/Back.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Back.module.css'
+
+export default function Back() {
+    return(
+        <div className={styles.BackButton}>
+            <div>chris RPE 8.5</div>
+        </div>
+    )
+}
