352f8a16081e03416c01ba620079c0213c65439b
han.lin <han.lin@mtahq.org>
2022-07-14 20:08:56 -0400

    Form
    Deployment reliant on form

---
+++ src/Form.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function RadioButton() {
+    return(
+        <div>
+            <input type='radio' value='Brooklyn' name='city'/>
+            <input type='radio' />
+
+        </div>  
+        
+    )
+}
